9cd4877f92fc58574e0e48a3f3688c7b0a955c64
jebalen <damian71253@gmail.com>
2023-05-17 20:31:44 +0300

    memory fix

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "../queue.h"
 #include "../tree.h"
-#include "xxhash.h"
-#include "stdlib.h"
-#include "string.h"
+#include <xxhash.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 //TODO
@@ -22,26 +22,23 @@
 // 3. hash first names. DONE
 // 4. INIT rbtree. DONE
 // 5. insert hashed values to rbtree. DONE
-// 6. create red-black balancing
-// 7. queue duplicates.
-// 8. free up memory.
+// 6. create red-black balancing. ????
+// 7. queue duplicates. DONE
+// 8. fix memory leaks.
 
 typedef unsigned int __uintptr_t;
-
 typedef struct node {
     RB_ENTRY(node) entry;
     char *name;
+    uint64_t hash_key;
     TAILQ_HEAD(dup_head, node) duplicates;
     TAILQ_ENTRY(node) dup_node;
-};
+} node_t;
 
 int compare_names_hash(struct node *a, struct node *b){
-    uint64_t ah = XXH64(a->name, strlen(a->name), 0);
-    uint64_t bh = XXH64(b->name, strlen(b->name), 0);
-
-    if (ah < bh){
+    if (a->hash_key < b->hash_key){
         return -1;
-    }else if (ah > bh){
+    }else if (a->hash_key > b->hash_key){
         return 1;
     }else {
         return 0;
@@ -52,78 +49,121 @@ RB_HEAD(stud_tree, node) head = RB_INITIALIZER(&head);
 RB_GENERATE(stud_tree, node, entry, compare_names_hash)
 
 void print_tree(struct node *n) {
-    struct node *left, *right;
-
+    node_t *left, *right;
+    
     if (n == NULL) {
         printf("nil");
         return;
     }
+
     left = RB_LEFT(n, entry);
     right = RB_RIGHT(n, entry);
-
-    if(left == NULL && right == NULL){
-        printf("%d", n -> name);
+    if (left == NULL && right == NULL) {
+        printf("%lu", n->hash_key);
     }else{
-        printf("%d\n", n->name);
-		print_tree(left);
-        printf(",");
-        print_tree(right);
+        printf("%lu (", n->hash_key);
+        node_t *dup_iterator;
+        TAILQ_FOREACH(dup_iterator, &n->duplicates, dup_node) {
+            printf("%lu, ", dup_iterator->hash_key);
+        }
         printf(")");
     }
 
+    printf(", left: ");
+    print_tree(left);
+    printf(", right: ");
+    print_tree(right);
+
+}
+// total heap usage: 178 allocs, 134 frees, 12,639 bytes allocated.
+void free_memory(node_t *n){
+    if(n == NULL){
+        return;
+    }
+    
+    node_t *left = RB_LEFT(n, entry);
+    node_t *right = RB_RIGHT(n, entry);
+
+    TAILQ_HEAD(dup_head, node) * duplicates = &n->duplicates;
+    node_t *dup_node;
+    while ((dup_node = TAILQ_FIRST(duplicates)) != NULL){
+        TAILQ_REMOVE(duplicates, dup_node, dup_node);
+        free(dup_node->name);
+        free(dup_node);
+    }
+
+    RB_REMOVE(stud_tree, &head, n);
+    free(n->name);
+    free(n);
+
+    free_memory(left);
+    free_memory(right);
 }
 
 int main() {
     FILE *fp = fopen("../teamsarray.txt", "r");
-    struct stud_tree node;
-    RB_INIT(&node);
     if(fp == NULL) {
         printf("Error in opening file.\n");
         exit(1);
     }
+
+    RB_INIT(&head);
+
     char *line = NULL;
     size_t len = 0;
-    struct node *n;
+    node_t *n;
 
     while(getline(&line, &len, fp) != -1) {
         char *first_name = strtok(line, " \t\n");
-        //printf("%s \n", first_name);
-        uint64_t hash = XXH64(first_name, strlen(first_name), 0);
-        n = malloc(sizeof(struct node));
+        printf("Name: %s\n", first_name);
+
+        n = malloc(sizeof(node_t));
         n->name = malloc(strlen(first_name) + 1);
         strcpy(n->name, first_name);
-        struct node *dup_stud = RB_INSERT(stud_tree, &head, n);
+        
+        // uint64_t hash 
+        n->hash_key = XXH64(n->name, strlen(n->name), 0);
+        printf("Hash: %lu\n", n->hash_key);
+
+        node_t *dup_stud = RB_FIND(stud_tree, &head, n);
         if(dup_stud != NULL) {
             TAILQ_INSERT_TAIL(&dup_stud->duplicates, n, dup_node);
         } else {
             TAILQ_INIT(&n->duplicates);
+            RB_INSERT(stud_tree, &head, n);
         }
     }
-    RB_FOREACH(n, stud_tree, &head){
-        printf("%d\n", n->name);
-        struct node *dup_iterator;
-        TAILQ_FOREACH(dup_iterator, &n->duplicates, dup_node){
-            printf("%d, ", dup_iterator->name);
-        }
-    }
-    printf("\n");
+
+  
+    // RB_FOREACH(n, stud_tree, &head){
+    // //     printf("%u\n", n->name);
+    // //     node_t *dup_iterator;
+    // //     TAILQ_FOREACH(dup_iterator, &n->duplicates, dup_node){
+    // //         printf(", %lu ", dup_iterator->name);
+    // //     }
+    // //     printf("\n");
+    // }
+
     printf("stud_tree:\n");
     print_tree(RB_ROOT(&head));
     printf("\n");
 
+    // node_t *var;
+    // node_t *nxt;
+    // for(var = RB_MIN(stud_tree, &head); var != NULL; var = nxt){
+    //     var = RB_NEXT(stud_tree, &head, var);
+    //     RB_REMOVE(stud_tree, &head, var);
+    //     free(var->name);
+    //     free(var);
+    //     free(var->hash_key);
+    // }
 
-    // char s[] = "test";
-    // uint64_t hashTest = XXH64(s, strlen(s), 0);
-    // uint64_t hashTest1 = XXH64(s, strlen(s), 0);
-    // printf("%u\n", hashTest);
-    // printf("%u", hashTest1);
-    struct node *var;
-    struct node *nxt;
-    for(var = RB_MIN(stud_tree, &head); var != NULL; var = nxt){
-        var = RB_NEXT(stud_tree, &head, var);
-        RB_REMOVE(stud_tree, &head, var);
-        free(var);
-    }
+    free_memory(RB_ROOT(&head));
+
     fclose(fp);
-    exit(EXIT_SUCCESS);
+    if (line != NULL) {
+        free(line);
+    }
+
+    return 0;
 }
